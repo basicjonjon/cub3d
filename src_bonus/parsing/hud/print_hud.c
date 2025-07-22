37ba0fe778f4bd6f6f6174c133b7b4dd0cb30024
Jonatan Le Douaron <jle-doua@f6r2s9.clusters.42paris.fr>
2025-07-22 16:11:14 +0200

    debut hud

--- src_bonus/parsing/hud/print_hud.c
+++ src_bonus/parsing/hud/print_hud.c
@@ -6,7 +6,7 @@
 /*   By: jle-doua <jle-doua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 14:14:23 by jle-doua          #+#    #+#             */
-/*   Updated: 2025/07/22 14:50:38 by jle-doua         ###   ########.fr       */
+/*   Updated: 2025/07/22 16:02:37 by jle-doua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,25 @@
 void	put_hud_test(t_data *data, int x_start, int y_start, int size)
 {
 	int save_x;
+	int color;
+	t_texture batterie;
 
-	save_x = x_start -1;
+	batterie.path = "assets/xpm/hud/batterie/bat5.xpm";
+	create_img(data, &batterie);
+
+	save_x = x_start - 1;
 	while (y_start < size)
 	{
 		x_start = save_x;
 		while (++x_start < size)
-			ft_pixel_put(x_start, y_start, &data->img, 255);
+		{
+			color = get_texture_pixel(&batterie, x_start, y_start);
+			if (color > 0)
+			{
+				ft_pixel_put(x_start, y_start, &data->img, color);
+			}
+		}
+
 		y_start++;
 	}
 }
